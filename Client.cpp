daf8531fcedae6b61b66318bdaa04b84544f3409
Eugene <eugen6215555@gmail.com>
2020-09-16 14:42:11 +0300

    Added based functionality and completed succeed connect between program

---
+++ Client.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+enum cmds
+{
+    greet,
+    admin_msg,
+    user_msg,
+    date_time,
+    close_session,
+    error
+};
+
+struct message
+{
+    char command[100];
+};
+
+void writemessage(message cmd)
+{
+    ofstream f("f1", ostream::binary | ostream::app);
+    f.write((char*)&cmd, sizeof(cmd));
+    f.close();
+}
+
+cmds read_answer(int new_size)
+{
+    ifstream f("f2", ostream::binary | ostream::in);
+    f.seekg(new_size, ostream::beg);
+    cmds result;
+    f.read((char*)&result, sizeof(result));
+    f.close();
+    return result;
+}
+
+int lst_size_check()
+{
+    ifstream file("f2", ios::binary | ios::ate);
+    file.seekg(0, ostream::end);
+    int file_size = file.tellg();
+    file.close();
+    return file_size;
+}
+
+void write_to_adm()
+{
+    cout << "Ok, now you can write to admin:\n";
+    message message;
+    cin.getline(message.command, sizeof(message.command));
+    int new_size = lst_size_check();
+    writemessage(message);
+}
+
+int main()
+{
+    cout << "You're in CommandService, Welcome!\n\n";
+    cout << "List of command:\n\n 1) Greetings: (1)\n 2) Write to admin: (2)\n 3) Today's date and time: (3)\n 4) Close session: (4)\n";
+    message message;
+
+    while ("connect")
+    {
+        cout << "\nEnter your command:\n$ ";
+        cin.getline(message.command, sizeof(message.command));
+
+        int new_size = lst_size_check();
+        writemessage(message);
+
+        while (true)
+        {
+            int newSize = lst_size_check();
+            if (newSize > new_size)
+                break;
+        }
+        cmds response = read_answer(new_size);
+
+        switch (response)
+        {
+        case cmds::greet:
+            cout << "Hello user!\n";
+            break;
+        case cmds::admin_msg:
+            write_to_adm();
+            break;
+        case cmds::error:
+            cout << "Undefined command, please try again!\n";
+            break;
+            /*     case cmds::greet:
+                cout << "Hello user!\n";
+                break;
+            case cmds::greet:
+                cout << "Hello user!\n";
+                break; */
+        }
+    }
+    system("pause");
+}
---
+++ Server.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+enum cmds {
+    greet,
+    admin_msg,
+    user_msg,
+    date_time,
+    close_session,
+    error
+};
+
+struct message {
+    char command[100];
+};
+
+message readStudent(int new_size) {
+    ifstream f("f1", ostream::binary | ostream::in);
+    f.seekg(new_size);
+    message result;
+    f.read((char*)& result, sizeof(result));
+    f.close();
+    return result;
+}
+
+void writeScholarship(cmds data) {
+    ofstream f("f2", ostream::binary | ostream::app);
+    f.write((char*)& data, sizeof(data));
+    f.close();
+}
+
+/* void createFiles() {
+    ofstream f1("f1", ostream::binary);
+    ofstream f2("f2", ostream::binary);
+    f1.close();
+    f2.close();
+} */
+
+int lst_size_check() {
+    ifstream f("f1", ostream::binary);
+    f.seekg(0, ostream::end);
+    return f.tellg();
+}
+
+cmds process(message message) {
+    if (strlen(message.command) < 2 && message.command[0] == '1')
+        return cmds::greet;
+    if (strlen(message.command) < 2 && message.command[0] == '2')
+        return cmds::admin_msg;
+    return cmds::error;
+}
+
+
+
+int main() {
+    cout << "Server started!\n";
+    while (true) {
+        int new_size = lst_size_check();
+        while (true)
+        {
+            int newSize = lst_size_check();
+            if (newSize > new_size)
+                break;
+        }
+        message message = readStudent(new_size);
+        cout << message.command << " ";
+        cout << endl;
+        cmds result = process(message);
+        writeScholarship(result);
+    }
+}
