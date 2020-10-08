5d2d9b6d11b83357747b608e86aa366bfefbc59a
Eugene <60621694+khudysh@users.noreply.github.com>
2020-10-08 10:31:49 +0300

    Add files via upload

--- first_project/Client.cpp
+++ first_project/Client.cpp
@@ -1,121 +1,141 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <cstring>
 #pragma warning(disable : 4996) //_CRT_SECURE_NO_WARNINGS
+
 using namespace std;
+
+const int ID_LENGTH = 10;
+char ID[ID_LENGTH];
+
+	char alphabet[52] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+'h', 'i', 'j', 'k', 'l', 'm', 'n',
+'o', 'p', 'q', 'r', 's', 't', 'u',
+'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+'H', 'I', 'J', 'K', 'L', 'M', 'N',
+'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+'V', 'W', 'X', 'Y', 'Z' };
 
 enum cmds
 {
-    greet,
-    admin_msg,
-    user_msg,
-    date_time,
-    close_session,
-    error
+	greet,
+	admin_msg,
+	user_msg,
+	date_time,
+	close_session,
+	error
 };
 
 struct message
 {
-    char command[100];
-    int id;
+	char command[100];
+	char id[ID_LENGTH];
 };
 
 void write_cmd(message cmd)
 {
-    ofstream f("f1", ostream::binary | ostream::app);
-    f.write((char*)&cmd, sizeof(cmd));
-    f.close();
+	ofstream f("f1", ostream::binary | ostream::app);
+	f.write((char*)&cmd, sizeof(cmd));
+	f.close();
 }
 
 cmds read_answer(int new_size)
 {
-    ifstream f("f2", ostream::binary | ostream::in);
-    f.seekg(new_size, ostream::beg);
-    cmds result;
-    f.read((char*)&result, sizeof(result));
-    f.close();
-    return result;
+	ifstream f(ID, ostream::binary | ostream::in);
+	f.seekg(new_size, ostream::beg);
+	cmds result;
+	f.read((char*)&result, sizeof(result));
+	f.close();
+	return result;
 }
 
 int lst_size_check()
 {
-    ifstream file("f2", ostream::binary | ostream::ate);
-    file.seekg(0, ostream::end);
-    int file_size = file.tellg();
-    file.close();
-    return file_size;
+	ifstream file(ID, ostream::binary | ostream::ate);
+	file.seekg(0, ostream::end);
+	int file_size = file.tellg();
+	file.close();
+	return file_size;
 }
 
-void write_to_adm(int userID)
+void write_to_adm()
 {
-    cout << "Ok, now you can write to admin:\n";
-    message message;
-    cin.getline(message.command, sizeof(message.command));
-    message.id = userID;
-    int new_size = lst_size_check();
-    write_cmd(message);
+	cout << "Ok, now you can write to admin:\n";
+	message message;
+	cin.getline(message.command, sizeof(message.command));
+	strcpy(message.id, ID);
+	int new_size = lst_size_check();
+	write_cmd(message);
 }
 
 void todays_time_date()
 {
-    // Текущие дата/время используемой системы
-    time_t now = time(0);
-    // Преобразуем в структуру tm для локальной временной зоны
-    tm* localtm = localtime(&now);
-    cout << "Local date and time: " << asctime(localtm) << endl;
+	time_t now = time(0);
+	tm* localtm = localtime(&now);
+	cout << "Local date and time: " << asctime(localtm) << endl;
 }
 
 void close_ses()
 {
-    ofstream file("f1", ostream::trunc);
-    file.close();
-    exit(0);
+	ofstream file("f1", ostream::trunc);
+	file.close();
+	exit(0);
+}
+
+void get_id() {
+	srand(char(time(0)));
+    for (int i = 0; i < ID_LENGTH; i++)
+        ID[i] = alphabet[rand()%52];
 }
 
 int main()
 {
-    srand(time(0));
-    cout << "You're in CommandService, Welcome!\n\n";
-    cout << "List of command:\n\n 1) Greetings: (1)\n 2) Write to admin: (2)\n 3) Today's date and time: (3)\n 4) Close session: (4)\n";
-    message message;
-    message.id = rand() % 101;
-    int userID = message.id;
+	srand(time(0));
+	get_id();
+	cout << ID << "\n";
+	cout << "You're in CommandService, Welcome!\n\n";
+	cout << "List of command:\n\n 1) Greetings: (1)\n 2) Write to admin: (2)\n 3) Today's date and time: (3)\n 4) Close session: (4)\n";
+	message message;
+	strcpy(message.id, ID);
+	while ("connect")
+	{
+		cout << "\nEnter your command:\n$ ";
+		cin.getline(message.command, sizeof(message.command));
 
-    while ("connect")
-    {
-        cout << "\nEnter your command:\n$ ";
-        cin.getline(message.command, sizeof(message.command));
+		int new_size = lst_size_check();
+		write_cmd(message);
 
-        int new_size = lst_size_check();
-        write_cmd(message);
+		while ("check")
+		{
+			int newSize = lst_size_check();
+			if (newSize > new_size)
+				break;
+		}
 
-        while ("check")
-        {
-            int newSize = lst_size_check();
-            if (newSize > new_size)
-                break;
-        }
-
-        cmds response = read_answer(new_size);
-        switch (response)
-        {
-        case cmds::greet:
-            cout << "Hello user!\n";
-            break;
-        case cmds::admin_msg:
-            write_to_adm(userID);
-            break;
-        case cmds::error:
-            cout << "Undefined command, please try again!\n";
-            break;
-        case cmds::date_time:
-            todays_time_date();
-            break;
-        case cmds::close_session:
-            cout << "See you!\n";
-            close_ses();
-            break;
-        }
-    }
-    system("pause");
+		cmds response = read_answer(new_size);
+		switch (response)
+		{
+		case cmds::greet:
+			cout << "Hello user!\n";
+			break;
+		case cmds::admin_msg:
+			write_to_adm();
+			break;
+		case cmds::error:
+			cout << "Undefined command, please try again!\n";
+			break;
+		case cmds::date_time:
+			todays_time_date();
+			break;
+		case cmds::close_session:
+			cout << "See you!\n";
+			close_ses();
+			break;
+		default:
+			cout << "Undefined command, please try again!\n";
+			break;
+		}
+	}
+	system("pause");
 }
